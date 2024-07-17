1d2d13a391bbdaa40d56631d9a3274c0e5b19f43
LordSheep <commandersheep@fierysnow.com>
2024-07-16 20:17:20 -0400

    Player name (saving data between scenes)
    after completing the "Player name (saving data between scenes)" tasks

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Sets the script to be executed later than all default scripts
+// This is helpful for UI, since other things may need to be initialized before setting the UI
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    // Based on this video (at 2:08): https://www.youtube.com/watch?v=zahrwl1125k
+    public void GrabFromInputField(string input)
+    {
+        MainManager.Instance.inputText = input;
+    }
+}
